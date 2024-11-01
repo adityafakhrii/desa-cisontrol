3efefb7869979a4a9ca6ca43e5ab3ab38e89ccd3
Aditya CodePolitan <aditya@codepolitan.com>
2024-11-01 16:49:14 +0700

    [ADD] Login, Sidebar, and Fake User Data

---
+++ resources/views/layouts/__sidebar.blade.php
@@ -0,0 +1,95 @@
+    <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item sidebar-profile mt-4">
+            <a href="#">
+              <div>
+                <div class="mt-2">
+                  <h3 class="mb-1 profile-name">Desa Cisontrol</h3>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-cards-variant menu-icon"></i>
+              <span class="menu-title">eKTP (F.1.07)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-cards-variant menu-icon"></i>
+              <span class="menu-title">Statistics Kependudukan</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Kartu Keluarga</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Keterangan Kelahiran</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Master Kependudukan</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">NIK Bayi Sementara</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">No KK Sementara</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Permohonan Pindah</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title" style="font-size: 0.8rem">Pernyataan Perubahan Data</span>
+            </a>
+          </li>
+
+          @auth
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">{{ auth()->user()->name }}</span>
+            </a>
+          </li>
+          <li class="nav-item report-generator mt-4">
+
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-danger btn-icon-text w-100">
+                    Logout
+                </button>
+            </form>
+          </li>
+          @else
+          <li class="nav-item">
+            <a class="nav-link" href="/login">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Login</span>
+            </a>
+          </li>
+          @endauth
+
+        </ul>
+      </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Controllers\LogoutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controllers\DashboardController::class, 'index'])->middleware('auth');
+
+Route::get('login', [Controllers\LoginController::class, 'loginForm'])->name('login')->middleware('guest');
+
+Route::post('login', [Controllers\LoginController::class, 'authenticate'])->middleware('guest');
+
+Route::post('logout', Controllers\LogoutController::class, 'logout')->name('logout')->middleware('auth');
