364cf6a541ff2a8e39b89e8c9bee6c37805d6c54
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2024-11-03 02:17:05 +0700

    [UPDATE] Login UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Deed UI Admin</title>
   <!-- base:css -->
-  <link rel="stylesheet" href="../../../../vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="../../../../vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="../../../../css/vertical-layout-light/style.css">
+  <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="../../../../images/favicon.png" />
+  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
 </head>
 
 <body class="sidebar-icon-only">
@@ -24,40 +24,23 @@
       <div class="content-wrapper d-flex align-items-center auth px-0">
         <div class="row w-100 mx-0">
           <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+            <div class="auth-form-light text-center py-5 px-4 px-sm-5">
               <div class="brand-logo">
-                <img src="../../../../images/logo-dark.svg" alt="logo">
+                <h1 style="color: #000" class="font-weight-bold">Desa Cisontrol App </h1>
               </div>
-              <h4>Hello! let's get started</h4>
-              <h6 class="font-weight-light">Sign in to continue.</h6>
+              <h4>Selamat Datang!</h4>
+              <h6 class="font-weight-light">Masuk untuk melanjutkan</h6>
 
               <form action="{{ route('login') }}" method="POST" class="pt-3">
                 @csrf
                 <div class="form-group">
-                  <input type="email" name="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                  <input type="email" name="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
                 </div>
                 <div class="form-group">
                   <input type="password" name="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="mt-3">
-                  <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
-                </div>
-                <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                    <label class="form-check-label text-muted">
-                      <input type="checkbox" class="form-check-input">
-                      Keep me signed in
-                    </label>
-                  </div>
-                  <a href="#" class="auth-link text-black">Forgot password?</a>
-                </div>
-                <div class="mb-2">
-                  <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                    <i class="mdi mdi-facebook mr-2"></i>Connect using facebook
-                  </button>
-                </div>
-                <div class="text-center mt-4 font-weight-light">
-                  Don't have an account? <a href="register.html" class="text-primary">Create</a>
+                  <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">MASUK</button>
                 </div>
               </form>
             </div>
@@ -70,14 +53,14 @@
   </div>
   <!-- container-scroller -->
   <!-- base:js -->
-  <script src="../../../../vendors/js/vendor.bundle.base.js"></script>
+  <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
   <!-- endinject -->
   <!-- inject:js -->
-  <script src="../../../../js/off-canvas.js"></script>
-  <script src="../../../../js/hoverable-collapse.js"></script>
-  <script src="../../../../js/template.js"></script>
-  <script src="../../../../js/settings.js"></script>
-  <script src="../../../../js/todolist.js"></script>
+  <script src="{{ asset('js/off-canvas.js') }}"></script>
+  <script src="{{ asset('js/hoverable-collapse.js') }}"></script>
+  <script src="{{ asset('js/template.js') }}"></script>
+  <script src="{{ asset('js/settings.js') }}"></script>
+  <script src="{{ asset('js/todolist.js') }}"></script>
   <!-- endinject -->
 </body>
 
