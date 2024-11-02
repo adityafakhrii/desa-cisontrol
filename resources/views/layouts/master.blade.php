32b62ceaf1c5617cbfac63b64005b2a2dc10fbc2
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2024-11-03 01:48:50 +0700

    [FIX] Master template content wrapper & footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,6 +51,12 @@
         <div class="content-wrapper">
             @yield('content')
         </div>
+        <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024 <a href="https://www.bootstrapdash.com/" target="_blank">Desa Cisontrol</a>. All rights reserved.</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with Love</span>
+            </div>
+          </footer>
         <!-- content-wrapper ends -->
         <!-- partial -->
       </div>
